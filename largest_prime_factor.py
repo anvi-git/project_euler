7ca2a1e0121822e973af9f57db99cf6fa7e71cbd
Antonio Viscusi <139615741+anvi-git@users.noreply.github.com>
2024-07-25 08:50:08 +0200

    Update largest_prime_factor.py

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,3 +1,8 @@
+#Problem #3
+#The prime factor of 13195 are 5,7,13 and 29.
+#What is the largest prime factor of the number 600851475143?
+#link to the problem: https://projecteuler.net/problem=3
+
 import numpy as np
 
 number = 600851475143
