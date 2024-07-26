57cb0750a6d4a3e73515e1208c1a6c31c7c26170
Antonio Viscusi <139615741+anvi-git@users.noreply.github.com>
2024-07-26 14:30:32 +0200

    Update largest_prime_factor.py

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -3,8 +3,6 @@
 #What is the largest prime factor of the number 600851475143?
 #link to the problem: https://projecteuler.net/problem=3
 
-import numpy as np
-
 number = 600851475143
 divisors = []
 i = 1
@@ -18,4 +16,4 @@ for i in range(1,number+1):
 
 print("the list of divisors is: ")
 print(divisors)
-print("the largest prime factor is = ", np.max(divisors))
+print("the largest prime factor is = ", divisors[-1])
